13f97c8da2fbbb601954a828629811898296fb73
Martin Reinecke <martin@mpa-garching.mpg.de>
2025-08-28 14:02:31 +0200

    update version to 3.1.0

--- ets_fiber_assigner/_version.py
+++ ets_fiber_assigner/_version.py
@@ -1,10 +1,9 @@
 import subprocess
 
-__version__ = "3.0.0"
+__version__ = "3.1.0"
 
 try:
     tmp = subprocess.run(["git", "describe", "--dirty"], capture_output=True, text=True)
     __gitversion__ = tmp.stdout.rstrip()
 except:
     __gitversion__ = "unknown"
-
