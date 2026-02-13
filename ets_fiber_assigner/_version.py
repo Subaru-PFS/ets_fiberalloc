5d348bdaee6ce5feac80621649e3d8bcb624a795
Martin Reinecke <martin@mpa-garching.mpg.de>
2026-02-13 13:43:27 +0100

    first attempt at a recursive version reporting strategy

--- ets_fiber_assigner/_version.py
+++ ets_fiber_assigner/_version.py
@@ -7,3 +7,23 @@ try:
     __gitversion__ = tmp.stdout.rstrip()
 except:
     __gitversion__ = "unknown"
+
+def recursive_version_info():
+    import ics.cobraOps
+    import pfs.utils
+    print(f"ets_fiber_assigner {__version__} ({__gitversion__}), depending on {{")
+    try:
+        ics.cobraOps.recursive_version_info()
+    except:
+        try:
+            print(f"ics.cobraOps {ics.cobraOps.__version__}")
+        except:
+            print("ics.cobraOps (unknown)")
+    try:
+        pfs.utils.recursive_version_info()
+    except:
+        try:
+            print(f"pfs.utils {pfs.utils.__version__}")
+        except:
+            print("pfs.utils (unknown)")
+    print("}")
